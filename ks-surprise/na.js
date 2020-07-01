9d3888a7cd172601f42a19c3ddfbf478e5faed91
jhadev <joshappeldev@gmail.com>
2020-07-01 11:01:25 -0400

    update ks-surprise na

--- ks-surprise/na.js
+++ ks-surprise/na.js
@@ -1,16 +1,23 @@
 // first first in stock item
 
-const tags = document.querySelectorAll(".product-tile");
-let firstInStockItem;
-for (let i = 0; i < tags.length; i += 1) {
-  if (tags[i].getAttribute("data-availability") === "IN_STOCK") {
-    firstInStockItem = tags[i];
-    break;
+function findFirstInStockItem() {
+  const tags = document.querySelectorAll(".product-tile");
+  let firstInStockItem;
+
+  for (let i = 0; i < tags.length; i += 1) {
+    if (tags[i].getAttribute("data-availability") === "IN_STOCK") {
+      firstInStockItem = tags[i];
+      break;
+    }
   }
+
+  const itemId = firstInStockItem.getAttribute("id");
+
+  const childNodes = firstInStockItem.children;
+
+  return childNodes[0].lastElementChild.href;
 }
 
-const itemId = firstInStockItem.getAttribute("id");
+const item = findFirstInStockItem();
 
-const childNodes = firstInStockItem.children;
-
-console.log(childNodes[0].lastElementChild.href);
+return item;
