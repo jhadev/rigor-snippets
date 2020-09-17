5f28d2892a68f5aee282b46ed352053c1773fa9d
jhadev <joshappeldev@gmail.com>
2020-09-16 21:03:07 -0400

    add function to grab first non member item

--- coachoutlet/us.js
+++ coachoutlet/us.js
@@ -7,3 +7,29 @@ if (alerts) {
     alerts[i].click();
   }
 }
+
+document.querySelector(
+  '#cont > div:nth-child(2) > div > div.product-image > div.plp-exclusive-msg'
+);
+
+function getNonMemberItems() {
+  let items = document.querySelectorAll('#isMemberExclusive');
+
+  const nonMemberItems = [];
+
+  items.forEach((item) => {
+    if (item.getAttribute('value') === 'false') {
+      nonMemberItems.push(item);
+    }
+  });
+
+  let [firstItem] = nonMemberItems;
+
+  const parent = firstItem.parentNode;
+
+  console.log(parent.children);
+
+  parent.children[3].click();
+}
+
+getNonMemberItems();
